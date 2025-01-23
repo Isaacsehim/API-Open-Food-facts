c6aa2f2edd8a51862e58660a34377deccaf71a28
Isaac Sehim <isaacsehim@gmail.com>
2025-01-23 12:20:07 +0100

    uiux semiok scanner ko

--- Assets/script.js
+++ Assets/script.js
@@ -11,43 +11,43 @@ fetch(url)
             document.getElementById('nomProduit').innerText = "Produit non trouvé ou mauvais numéro de code-barre";
         } else {
     document.getElementById('informations').innerHTML =`<div class="nomProduit" id="nomProduit">
-        <h1>${data.product.product_name}</h1>
+        <h2>${data.product.product_name}</h2>
       </div>
       <div class="photoProduit" id="photoProduit">
-        <img src="${data.product.image_front_small_url}" alt="photoproduitscanner">
+        <img src="${data.product.image_front_small_url == null || data.product.image_front_small_url == 'unknown' || data.product.image_front_small_url == 'not-applicable' ? "img/Green/green-score-IDK.svg" : `${data.product.image_front_small_url}`}" alt="photoproduitscanner"></img>
       </div>
       <div class="code-barre" id="code-barre">
-        <h2>Code-barre</h2>
-        <p>${data.product.code == null || data.product.code == "" || data.product.code == 'unknown' ? "Non renseigné" : data.product.code}</p>
+        <h3>Code-barre</h3>
+        <p>${data.product.code == null || data.product.code == "" || data.product.code == 'unknown' || data.product.code == 'not-applicable' ? "Non renseigné" : data.product.code}</p>
       </div>
       <div class="dénomination-générique" id="dénomination-générique">
-        <h2>Dénomination générique</h2>
-        <p>${data.product.generic_name_fr == null || data.product.generic_name_fr == "" || data.product.generic_name_fr == 'unknown' ? "Non renseigné" : data.product.generic_name_fr}</p>
+        <h3>Dénomination générique</h3>
+        <p>${data.product.generic_name_fr == null || data.product.generic_name_fr == "" || data.product.generic_name_fr == 'unknown' || data.product.generic_name_fr == 'not-applicable' ? "Non renseigné" : data.product.generic_name_fr}</p>
       </div>
       <div class="quantité" id="quantité">
-        <h2>Quantité</h2>
-        <p>${data.product.quantity == null || data.product.quantity == "" || data.product.quantity == 'unknown' ? "Non renseigné" : data.product.quantity}</p>
+        <h3>Quantité</h3>
+        <p>${data.product.quantity == null || data.product.quantity == "" || data.product.quantity == 'unknown' || data.product.quantity == 'not-applicable' ? "Non renseigné" : data.product.quantity}</p>
       </div>
       <div class="marque" id="marque">
-        <h2>Marque</h2>
-        <p>${data.product.brands == null || data.product.brands == "" || data.product.brands == 'unknown' ? "Non renseigné" : data.product.brands}</p>
+        <h3>Marque</h3>
+        <p>${data.product.brands == null || data.product.brands == "" || data.product.brands == 'unknown' || data.product.brands == 'not-applicable' ? "Non renseigné" : data.product.brands}</p>
       </div>
       <div class="catégorie" id="catégorie">
-        <h2>Catégorie</h2>
-        <p>${data.product.categories == null || data.product.categories == "" || data.product.categories == 'unknown' ? "Non renseigné" : data.product.categories}</p>
+        <h3>Catégorie</h3>
+        <p>${data.product.categories == null || data.product.categories == "" || data.product.categories == 'unknown' || data.product.categories == 'not-applicable' ? "Non renseigné" : data.product.categories}</p>
       </div>
         <div class="nutriscore" id="nutriscore">
-        <h2>Nutriscore</h2>
-                <img src="${data.product.nutriscore_grade == null || data.product.nutriscore_grade == 'unknown' ? "img/nutri/IDK.svg" : `img/nutri/${data.product.nutriscore_grade}.svg`}" alt="PhotoNovascore"></img>
+        <h3>Nutriscore</h3>
+        <img src="${data.product.nutriscore_grade == null || data.product.nutriscore_grade == 'unknown' || data.product.nutriscore_grade == 'not-applicable' ? "img/nutri/IDK.svg" : `img/nutri/${data.product.nutriscore_grade}.svg`}" alt="PhotoNovascore"></img>
         <p></p>
       </div>
       <div class="novascore" id="novascore">
-      <h2>Novascore</h2>
-        <img src="${data.product.nova_group == null || data.product.nova_group == 'unknown' ? "img/nova/NovaIDK.svg" : `img/nova/Nova${data.product.nova_group}.svg`}" alt="PhotoNovascore"></img>
+      <h3>Novascore</h3>
+        <img src="${data.product.nova_group == null || data.product.nova_group == 'unknown' || data.product.nova_group == 'not-applicable' ? "img/nova/NovaIDK.svg" : `img/nova/Nova${data.product.nova_group}.svg`}" alt="PhotoNovascore"></img>
         <p></p>
       </div>
       <div class="greenscore" id="greenscore">
-        <h2>Greenscore</h2>
-        <img src="${data.product.ecoscore_grade == null || data.product.ecoscore_grade == 'unknown'? "img/Green/green-score-IDK.svg" : `img/Green/green-score-${data.product.ecoscore_grade}.svg`}" alt="PhotoGreenscore"></img>
+        <h3>Greenscore</h3>
+        <img src="${data.product.ecoscore_grade == null || data.product.ecoscore_grade == 'unknown' || data.product.ecoscore_grade == 'not-applicable' ? "img/Green/green-score-IDK.svg" : `img/Green/green-score-${data.product.ecoscore_grade}.svg`}" alt="PhotoGreenscore"></img>
       </div>`;
     }});
